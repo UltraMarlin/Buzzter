5ee04dec552361b43dad3ffd21357b6d0d096eef
UltraMarlin <68075515+UltraMarlin@users.noreply.github.com>
2023-08-01 20:53:48 +0200

    Initialize Structure

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple Socket.io based Buzzer Application that was developed for the Week of Charity 2023
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
